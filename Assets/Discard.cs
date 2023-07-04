b0c13efbf67871b16351416dc20208a8a974f763
ymstmgm <134988637+ymstmgm@users.noreply.github.com>
2023-07-04 14:34:16 +0900

    再提出

---
+++ Assets/Discard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Discard : MonoBehaviour
+{
+
+    private Transform playerTransform;
+
+    private void Start()
+    {
+        // unityちゃんの位置を取得
+        playerTransform = GameObject.Find("unitychan").transform;
+    }
+
+    private void Update()
+    {
+        // unityちゃんが通り過ぎた後にアイテムを破棄
+        if (playerTransform.position.z > transform.position.z +10f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
